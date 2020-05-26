665a74d8e194e350f198fa5ebf2d0b267da7b765
Corenb <Corenb@users.noreply.github.com>
2020-05-27 01:49:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,37 @@
-screen -dmS survie java -Xms2G -Xmx14336M -Dfile.encoding=UTF8 -Dterminal.jline=false -Dterminal.ansi=true -jar /srv/survie/server.jar nogui
+# Obfuscation script to pull from Github and replace $variables
+
+# Variables
+ORIGIN=origin
+BRANCH=master
+
+source secret.dat
+
+# Pull from Github
+git pull $ORIGIN $BRANCH
+
+# File list
+config_files=("server.properties")
+
+i=0
+
+# Loop upto size of array
+# starting from index, i=0
+while [ $i -lt ${#config_files[@]} ]
+do
+    # To print index, ith
+    # element
+    echo "Deobfuscating ${config_files[$i]} file..."
+
+    for key in "${!values[@]}"
+    do
+      sed -i "s/$key/${values[$key]}/g" ${config_files[$i]}
+#      echo "$key => ${values[$key]}"
+    done
+
+
+    # Increment the i = i + 1
+    i=`expr $i + 1`
+done
+
+# Start server
+java -Xms18432M -Xmx18432M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
