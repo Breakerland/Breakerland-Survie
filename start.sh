87d064b34622e59382c7386190e60fba662601a2
Corentin Boiteau <Corenb@users.noreply.github.com>
2020-07-01 20:21:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Obfuscation script to pull from Github and replace $variables
 
 # Variables
@@ -9,29 +10,17 @@ source secret.dat
 # Pull from Github
 git pull 
 
-# File list
-config_files=("server.properties")
-
-i=0
-
-# Loop upto size of array
-# starting from index, i=0
-while [ $i -lt ${#config_files[@]} ]
+echo "Starting to deobfuscate files..."
+shopt -s nullglob
+for i in server.properties plugins/*/*.yml;
 do
-    # To print index, ith
-    # element
-    echo "Deobfuscating ${config_files[$i]} file..."
-
-    for key in "${!values[@]}"
-    do
-      sed -i "s/$key/${values[$key]}/g" ${config_files[$i]}
-#      echo "$key => ${values[$key]}"
+    for key in "${!secret_key[@]}"
+    do 
+      sed -i "s/$key/${secret_key[$key]}/g" $i
     done
-
-
-    # Increment the i = i + 1
-    i=$(( $i + 1 ))
 done
+echo "Deobfuscation complete."
 
 # Start server
-java -Xms18432M -Xmx18432M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
+echo "Starting server..."
+java -Xms$1M -Xmx$1M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
