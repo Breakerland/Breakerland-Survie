aa496c9c5743229e66edd8415de83708c6f58c93
Corenb <Corenb@users.noreply.github.com>
2020-10-20 02:36:13 +0200

    Update plugins settings

--- plugins/dynmap/web/standalone/MySQL_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbname = 's1_survie';
-$dbhost = '172.18.0.1';
-$dbport = 3306;
-$dbuserid = 'u1_9CoCZ2vYY6';
-$dbpassword = 'r5stVAYZrSErdNSniT2K2sf5';
-$dbprefix = 'dynmap_';
-$loginenabled = false;
-?>
--- plugins/dynmap/web/version.js
+++
@@ -1,2 +0,0 @@
-var dynmapversion = "3.1-beta-1-367";
-
